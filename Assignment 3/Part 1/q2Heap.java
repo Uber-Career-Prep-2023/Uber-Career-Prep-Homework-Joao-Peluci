9c043bc80c94791d30a69fda5f46cf100e34ace6
Joao Peluci <jvpeluci@gmail.com>
2023-06-22 19:55:35 -0300

    Add: solve Q2Heap

--- Assignment 3/Part 1/q2Heap.java
+++ Assignment 3/Part 1/q2Heap.java
@@ -1,47 +1,74 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class q2Heap {
-    List<Integer> arr = new ArrayList<>();
-    
-    public int top(){ //returns the min (top) element in the heap
-        return 0;
-    }; 
-    public void insert(int x){
-        int index = 0;
-        if(this.arr.size()==0){
-            this.arr.add(x);
-        }
-        else{
-            while (x>this.arr.indexOf(index)){
-                
-            }
-        }
-    };
-    public void remove(){ //removes the min (top) element in the heap
+    int capacity = 8;
+    ArrayList<Integer> heap = new ArrayList<>();
 
+    public int top() { // returns the min (top) element in the heap
+        return heap.get(0);
     };
-    public void heapify(){
-        
+
+    public void insert(int x) {
+        heap.add(x);
+        heapify();
+    };
+
+    public void remove() { // removes the min (top) element in the heap
+        heap.remove(heap.size()-1);
+        heapify();
+    };
+    public void heapify() {
+        int child = heap.size()-1;
+        int parent;
+
+        // System.out.println("Iniciando funcao heapify");
+        // System.out.println("Filho:"+heap.get(child));
+
+        while (child != 0) {
+            if (child % 2 != 0) {
+                parent = (child - 1) / 2;
+                // System.out.println("Filho impar, pai:"+parent);
+            }
+            else{
+                parent = child / 2;
+                // System.out.println("Filho par, Pai:"+parent);
+            }
+
+            if (heap.get(parent) > heap.get(child)) {
+                // System.out.println("Invertendo a posicao");
+                int tmp = heap.get(child);
+                heap.set(child, heap.get(parent));
+                heap.set(parent, tmp);
+            }
+            child = parent;
+        }
     }
 
     public static void main(String[] args) {
         q2Heap heap = new q2Heap();
-
-        // Insert elements into the heap
-        heap.insert(5);
-        heap.insert(8);
-        heap.insert(3);
+          
+        heap.insert(42);
+        heap.insert(31);
         heap.insert(10);
-        heap.insert(1);
+        heap.insert(35);
+        heap.insert(44);
+        heap.insert(14);
+        heap.insert(33);
+        heap.insert(26);
+        heap.insert(27);
+        heap.insert(19);
+        for (Integer i : heap.heap) {
+                System.out.println(i);
+        }
+
 
         // Print the top element
-        System.out.println("Top element: " + heap.top()); // Expected output: 1
+        // System.out.println("Top element: " + heap.top()); // Expected output: 1
 
         // Remove the top element
-        heap.remove();
+        // heap.remove();
 
         // Print the new top element
-        System.out.println("New top element: " + heap.top()); // Expected output: 3
+        // System.out.println("New top element: " + heap.top()); // Expected output: 3
     }
 }
